b5f83520d34dd39679d73d21cfd2a775559abf33
Ryan Lindsey <ryan.lindsey44@gmail.com>
2019-04-17 16:25:18 -0400

    Checking in. Able to open the file with an absolute path, but I'd like to find a good way to run tests and have rust find the file and open it based on a relative path (copying/opening files in the tests directory, for example).

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate gameboy;
 
-use gameboy::cpu::cpu::*;
+use std::fs;
+use std::path::Path;
+//use gameboy::cpu::cpu::*;
 
 fn main() {
     /*
      * TODO: Open file in test directory.
      */
     let dir = String::from("C:\\Code\\rust\\gameboy\\data\\cpu_instrs\\individual\\01-special.gb");
-
+    let data: Vec<u8> = fs::read(dir).expect("Unable to read file"); 
+    println!("{:?}", data);
 }
