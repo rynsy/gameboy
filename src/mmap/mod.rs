b4a1c3d52f7fb256748b0f0e3f3da66eb9d0ef7d
Ryan Lindsey <ryan.lindsey44@gmail.com>
2019-04-19 13:30:46 -0400

    added basic mmap functions

--- src/mmap/mod.rs
+++ src/mmap/mod.rs
@@ -4,8 +4,39 @@ pub struct MMap {
 
 impl Default for MMap {
     fn default() -> MMap {
+        let vec: Vec<u16> = vec![0; 0xFFFF];
         MMap{
-            data: Vec::with_capacity(0xFFFF),
+            data: vec,
         }
+    }
+}
+
+impl MMap {
+    pub fn write(&mut self, addr: u16, val: u16 ) {   // TODO: Value may be u8
+        self.data[addr as usize] = val;
+    }
+
+    pub fn read(&self, addr: u16 ) -> u16 { // TODO: May return u8
+        self.data[addr as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MMap;
+
+    #[test]
+    fn test_write() {
+        let mut a: MMap = Default::default();
+        a.write(0, 10);
+    }
+
+    #[test]
+    fn test_read() {
+        let val = 10;
+        let mut a: MMap = Default::default();
+        a.write(0, val);
+        let b = a.read(0);
+        assert_eq!(val, b);
     }
 }
