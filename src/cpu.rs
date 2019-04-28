cb95eada63194bd1f84eb7c624f77a8f7bd8de9f
Ryan Lindsey <ryan.lindsey44@gmail.com>
2019-04-28 15:28:28 -0400

    Added the last of the general ALU functions. Removed some that are specific applications of the general functions. Documented all of the rotation functions

--- src/cpu.rs
+++ src/cpu.rs
@@ -131,7 +131,7 @@ impl CPU {
     }
 
     fn alu_add_hw_imm(&mut self, v: u16) -> u16 {
-        let a = self.imm() as i8 as i16 as u16;  // TODO This is signed, need to convert
+        let a = self.imm() as i8 as i16 as u16;  // TODO This is signed, test this conversion. 
         self.set_flag(Z, false);
         self.set_flag(N, false);
         self.set_flag(H, ((a & 0x000F) + (v & 0x000F)) > 0x000F );
@@ -139,14 +139,15 @@ impl CPU {
         a.wrapping_add(v)
     }
 
-    fn alu_inc_hw(&mut self, v: u16) -> u16 {
-        v.wrapping_add(1)    
-    }
-
-    fn alu_dec_hw(&mut self, v: u16) -> u16 {
-        v.wrapping_sub(1)    
-    }
-
+    /*
+     *  Swap upper/lower nibbles of n
+     *  n = A,B,C,D,E,H,L,(HL)
+     *
+     *  Z - set if res = 0
+     *  N - Reset
+     *  H - Reset
+     *  C - Reset
+     */
     fn alu_swap(&mut self, v: u8) -> u8 {
         self.set_flag(Z, v == 0);
         self.set_flag(N, false);
@@ -155,6 +156,15 @@ impl CPU {
         (v >> 4) | (v << 4)
     }
 
+    /*
+     *  Decimal Adjust register A
+     *  Adjust A so that it's in Binary Coded Decimal (BCD) format
+     *
+     *  Z - Set if A = 0
+     *  N - N/A
+     *  H - Reset 
+     *  C - Set/Reset according to operation
+     */
     fn alu_daa(&mut self) {
         // convert register A to BCD
         let mut a = self.reg.a;
@@ -174,130 +184,206 @@ impl CPU {
         self.set_flag(C, adjust >= 0x60);
     }
 
+    /*
+     *  Complement Register A
+     *
+     *  Z - N/A
+     *  N - Set
+     *  H - Set 
+     *  C - N/A
+     */
     fn alu_cpl(&mut self) {
         self.reg.a = !(self.reg.a);
     }
 
+    /*
+     *  Complement Carry Flag (C = !C)
+     *
+     *  Z - N/A
+     *  N - Reset
+     *  H - Reset
+     *  C - Complemented
+     */
     fn alu_ccf(&mut self) {
         self.set_flag(N, false);
         self.set_flag(H, false);
         self.set_flag(C, !self.flag_c());
     }
 
+    /*
+     *  Set Carry Flag
+     *
+     *  Z - N/A
+     *  N - Reset
+     *  H - Reset
+     *  C - Set
+     */
     fn alu_scf(&mut self) {
         self.set_flag(N, false);
         self.set_flag(H, false);
         self.set_flag(C, true);
     }
 
-    fn alu_rlca(&mut self) {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-    }
-
-    fn alu_rla(&mut self) {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-    }
-
-    fn alu_rrca(&mut self) {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-    }
-
-    fn alu_rra(&mut self) {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-    }
-
+    /*
+     * Rotate A left, old bit 7 to carry flag
+     *
+     *  Z - Set if res = 0
+     *  N - Reset
+     *  H - Reset
+     *  C - Contains old bit 7 data
+     */
     fn alu_rlc(&mut self, v: u8) -> u8 {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-        0
-    }
-
-    fn alu_rl(&mut self, v: u8) -> u8 {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-        0
-    }
-
-    fn alu_rrc(&mut self, v: u8) -> u8 {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-        0
-    }
-
-    fn alu_rr(&mut self, v: u8) -> u8 {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-        0
-    }
-
-    fn alu_sla(&mut self, v: u8) -> u8 {
-        //TODO writeme
-        self.set_flag(Z, false);
-        self.set_flag(N, false);
-        self.set_flag(H, false);
-        self.set_flag(C, true);
-        0
-    }
-
-    fn alu_sra(&mut self, v: u8) -> u8 {
-        // NOTE: May not need to return the result. 
-        // Don't know if the result is saved in the
-        //  register or not.
-        self.set_flag(C, (1 & v) != 0 );
-        let res = v >> 1;
+        //TODO check this
+        let c_res = (v & (1 << 7)) != 0;
+        let res = v << 1;
         self.set_flag(Z, res == 0 );
         self.set_flag(N, false);
         self.set_flag(H, false);
+        self.set_flag(C, c_res );
         res
     }
 
-    fn alu_srl(&mut self, v: u8) -> u8 {
-        // NOTE: May not need to return the result. 
-        // Don't know if the result is saved in the
-        //  register or not.
-        //
-        //  TODO: set MSB = 0
-        self.set_flag(C, (1 & v) != 0 );
-        let res = v >> 1;
+    /*
+     * Rotate A left through carry flag
+     *
+     *  Z - Set if res = 0
+     *  N - Reset
+     *  H - Reset
+     *  C - Contains old bit 0 data
+     */
+    fn alu_rl(&mut self, v: u8) -> u8 {
+        let c_res = (v & (1 << 7)) != 0;
+        let res = v << 1 + u8::from(self.flag_c());
         self.set_flag(Z, res == 0 );
         self.set_flag(N, false);
         self.set_flag(H, false);
+        self.set_flag(C, c_res );
+        res
+    }
+
+    /*
+     * Rotate A right, old bit 0 to carry flag
+     *
+     *  Z - Set if res = 0
+     *  N - Reset
+     *  H - Reset
+     *  C - Contains old bit 0 data
+     */
+    fn alu_rrc(&mut self, v: u8) -> u8 {
+        let c_res = (v & 0x01) == 0x01;
+        let res = if c_res { 0x80 | (v >> 1) } else { v >> 1 };
+        self.set_flag(Z, res == 0 );
+        self.set_flag(N, false);
+        self.set_flag(H, false);
+        self.set_flag(C, c_res);
+        res
+    }
+
+    /*
+     * Rotate A right through carry flag
+     *
+     *  Z - Set if res = 0
+     *  N - Reset
+     *  H - Reset
+     *  C - Contains old bit 0 data
+     */
+    fn alu_rr(&mut self, v: u8) -> u8 {
+        let res = if self.flag_c() { 0x80 | (v >> 1) } else { v >> 1 };
+        self.set_flag(Z, res == 0 );
+        self.set_flag(N, false);
+        self.set_flag(H, false);
+        self.set_flag(C, (v & 0x01) == 0x01 );
+        res
+    }
+
+    /*
+     * Shift n left into Carry. LSB = 0
+     *  n = A,B,C,D,E,H,L,(HL) 
+     *
+     *  Z - Set if res = 0
+     *  N - Reset
+     *  H - Reset
+     *  C - Contains old bit 7 data
+     */
+    fn alu_sla(&mut self, v: u8) -> u8 {
+        let c_res = (v & (1 << 7)) != 0;
+        let res = v << 1;
+        self.set_flag(Z, res == 0 );
+        self.set_flag(N, false);
+        self.set_flag(H, false);
+        self.set_flag(C, c_res );
+        res
+    }
+
+    /*
+     * Shift n right into Carry. MSB unchanged
+     *  n = A,B,C,D,E,H,L,(HL) 
+     *
+     *  Z - Set if res = 0
+     *  N - Reset
+     *  H - Reset
+     *  C - Contains old bit 0 data
+     */
+    fn alu_sra(&mut self, v: u8) -> u8 {
+        let c_res = (1 & v) != 0;
+        let res = (v >> 1) | (v & (1 << 7));
+        self.set_flag(Z, res == 0 );
+        self.set_flag(N, false);
+        self.set_flag(H, false);
+        self.set_flag(C, c_res );
+        res
+    }
+
+    /*
+     * Shift n right into Carry. MSB = 0
+     *  n = A,B,C,D,E,H,L,(HL) 
+     *
+     *  Z - Set if res = 0
+     *  N - Reset
+     *  H - Reset
+     *  C - Contains old bit 0 data
+     */
+    fn alu_srl(&mut self, v: u8) -> u8 {
+        let c_res = (1 & v) != 0;
+        let res = (v >> 1) & !(1 << 7); //MSB may be zero from shift, doing it manually just cause 
+        self.set_flag(Z, res == 0 );
+        self.set_flag(N, false);
+        self.set_flag(H, false);
+        self.set_flag(C, c_res );
         res 
     }
 
+    /*
+     *  Test bit (b) in register (r)
+     *
+     *  Z - Set if b of r == 0
+     *  N - Reset
+     *  H - Set
+     *  C - N/A
+     */
     fn alu_bit(&mut self, b: u8, r: u8) {
         // NOTE: Handle the (HL) case in ex()
         self.set_flag(Z, ((1 << b) & r) == 0 );
         self.set_flag(N, false);
         self.set_flag(H, true);
+    }
+
+    /*
+     *  Set bit (b) in register (r)
+     */
+    fn alu_set(&self, b: u8, r: u8) -> u8 {
+        (1 << b) | r
+    }
+
+    /*
+     *  Reset bit (b) in register (r)
+     */
+    fn alu_reset(&self, b: u8, r: u8) -> u8 {
+        !(1 << b) & r
+    }
+
+    fn alu_jr(&mut self) {
+        // TODO writeme
     }
 
     pub fn set_flag(&mut self, flag: Flag, val: bool) {
